84b92c4bddaa447011c86f86700d1fd64948c86f
Alex Gorovyi <gorovyi.ov@gbksoft.com>
2014-12-31 11:52:46 +0200

    wp-content/themes/iusedautoparts/js/site.js

--- wp-content/themes/iusedautoparts/js/site.js
+++ wp-content/themes/iusedautoparts/js/site.js
@@ -27,13 +27,16 @@ $( document ).ready(function() {
 
     $(".vehicle").click(function () { 
         var req = $(this).data('request');
-        req['test'] = 'test';
-        $.post( "inventory", req, function( data ) {
-            var ht = $(data).find('.table-hide');
-            $('.table-hide').replaceWith(ht);    
+        req['addres'] = 'addres';
+        req['yardid'] = $(this).data('id');
+//        var _self = $(this).parents('td').parent('tr');
+        $.post( "/testing/ajax/add_result.php", req, function( data ) {
+                var namevendor = $(data).find('.namevendor:first').text();
+                $('#data').find('.modal-body').html(data);
+                $('#data').find('#myModalLabel span').text(namevendor);
+                $('#data').modal('show');
         });  
     });        
-    
 });
 
 
@@ -564,7 +567,7 @@ function InitPrimarySearch() {
     });
     $("#btn-new-search").click(function (e) {
         e.preventDefault();
-        window.location = "http://www.iusedautoparts.dev.gbksoft.net";
+        window.location = "/";
 
     });
 }
@@ -924,91 +927,6 @@ function updateRequestContactData() {
         alert("Please enter a valid phone number");
         return false;
     }
-
-//  
-//    var params;
-//
-//    var optvalue = $("#optionvalue").val();
-//    var hoption;
-//    if (optvalue) {
-//        hoption = optvalue.label;
-//        if (optvalue.parentElement) {
-//
-//            var parent = $('#optionvalue').jqxTree('getItem', optvalue.parentElement);
-//            hoption += "," + parent.label;
-//            if (parent.parentElement) {
-//                var grandparent = $('#optionvalue').jqxTree('getItem', parent.parentElement);
-//                hoption = grandparent.label + "," + hoption;
-//                if (grandparent.parentElement) {
-//                    var greatgrandparent = $('#optionvalue').jqxTree('getItem', grandparent.parentElement);
-//                    hoption = greatgrandparent.label + "," + hoption;
-//                }
-//            }
-//        }
-//    }
-//    else
-//        hoption = "";
-//
-//    var optvalue = document.getElementById("hollanderoption").value;
-//    if (document.getElementById('preload-partname'))
-//        carPartName = document.getElementById('preload-partname').value;
-//    else if (document.getElementById('partname').value != "")
-//        carPartName = document.getElementById('partname').value;
-//    else if (document.getElementById('preload-chpartname'))
-//        carPartName = document.getElementById('preload-chpartname').value;
-//    if (cars.selectedCarYear && cars.selectedManufacture && cars.selectedModel &&
-//            carPartName && optvalue && document.getElementById('zip').value) {
-//        //alert("submitting");
-//        //continue;
-//    } else
-//        return false;
-//    params = {
-//        year: cars.selectedCarYear,
-//        make: cars.selectedManufacture,
-//        model: cars.selectedModel,
-//        partname: carPartName,
-//        interchange: optvalue,
-//        hollanderoption: hoption,
-//        zip: document.getElementById('zip').value};
-//    var response;
-//    $.ajax({
-//        async: false,
-//        type: 'post',
-//        url: "http://www.iusedautoparts.dev.gbksoft.net/scripts/request.php",
-//        data: params,
-//        success: function(resp) {
-//            if (resp)
-//            {
-//                response = resp;
-//            }
-//            else
-//                response = false;
-//        }
-//    });
-//    var reqid = response;
-//    document.getElementById('reqid').value = reqid;
-//
-//    var progress = setInterval(function() {
-//        var $bar = $('.bar');
-//
-//        if ($bar.width() > 500) {
-//
-//            //window.location.href = "/inventory";
-//
-//            window.searchform.submit();
-//            clearInterval(progress);
-//            $('.progress').removeClass('active');
-//            $('#modal-progress').modal('hide');
-//            $bar.width(0);
-//
-//        } else {
-//
-//
-//            $bar.width($bar.width() + 50);
-//        }
-//
-//        $bar.text($bar.width() / 5 + "%");
-//    }, 800);
 }
 
 window.onkeydown = function (e) {
@@ -1049,18 +967,7 @@ window.onkeydown = function (e) {
 
         return false;
 
-        /*    if(document.getElementById('zip').value.length != 5 || document.getElementById("group-button").style.display!="none") {
-         alert("Please provide a five-digit zip code.");
-         } else {
-         $('#modal-progress').modal({
-         backdrop: 'static',
-         show: true
-         });
-         }
-         */
         return false;
-        //alert("test");
-        //alert("again");
     }
 }
 
